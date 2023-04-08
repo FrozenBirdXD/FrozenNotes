e55857c80e1ea6aa4240d41e2a5b5273511a55e4
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-04-08 21:13:27 +0200

    refactor and homepage notes logic

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,26 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frozennotes/views/login_view.dart';
+import 'package:frozennotes/views/notes_view.dart';
 import 'package:frozennotes/views/register_view.dart';
 import 'package:frozennotes/views/verify_email_view.dart';
-import 'firebase_options.dart';
+import 'package:frozennotes/firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
-        title: 'Frozennotes',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomePage(),
-        routes: {
-          '/login/': (context) => const LoginView(),
-          '/register/': (context) => const RegisterView(),
-        }),
+      title: 'Frozennotes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      }
+    ),
   );
 }
 
@@ -37,16 +40,19 @@ class HomePage extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                print('Email is verified');
+                return const NotesView();
               } else {
                 return const VerifyEmailView();
               }
             } else {
               return const LoginView();
             }
-            return const Text('Done');
           default:
-            return const CircularProgressIndicator();
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              )
+            );
         }
       },
     );
