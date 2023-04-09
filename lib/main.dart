1b749c14658a39a571de2f6703221ee4e1dbff2d
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-04-09 13:59:38 +0200

    fix routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:frozennotes/constants/routes.dart';
 import 'package:frozennotes/views/login_view.dart';
 import 'package:frozennotes/views/notes_view.dart';
 import 'package:frozennotes/views/register_view.dart';
@@ -18,9 +19,10 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-      }
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
+      },
     ),
   );
 }
@@ -51,7 +53,7 @@ class HomePage extends StatelessWidget {
             return const Scaffold(
               body: Center(
                 child: CircularProgressIndicator(),
-              )
+              ),
             );
         }
       },
