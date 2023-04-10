74bda5f43bc84ea42a6654bf52ea8ec0a752c0c9
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-04-10 11:07:45 +0200

    implement auth_expections to abstract

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,15 @@
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+// generic exceptions
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthExpections implements Exception {}
