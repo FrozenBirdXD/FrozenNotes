10fb8514081626086b67c8ffbc294f2fcf44e56d
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-04-11 17:18:32 +0200

    use AuthService instead of firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frozennotes/constants/routes.dart';
+import 'package:frozennotes/services/auth/auth_service.dart';
 import 'package:frozennotes/views/login_view.dart';
 import 'package:frozennotes/views/notes_view.dart';
 import 'package:frozennotes/views/register_view.dart';
 import 'package:frozennotes/views/verify_email_view.dart';
-import 'package:frozennotes/firebase_options.dart';
 import 'dart:developer' as devtools show log;
 
 void main() {
@@ -34,15 +32,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      ),
+      future: AuthService.firebase().initialize(),
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            final user = FirebaseAuth.instance.currentUser;
+            final user = AuthService.firebase().currentUser;
             if (user != null) {
-              if (user.emailVerified) {
+              if (user.isEmailVerified) {
                 return const NotesView();
               } else {
                 return const VerifyEmailView();
