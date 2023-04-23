4c8f9abb0b4f3642b4e60c39c8fd6f4afa74f015
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-04-23 21:43:35 +0200

    add CloudNote for notes

---
+++ lib/services/cloud/cloud_note.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:frozennotes/services/cloud/cloud_storage_constants.dart';
+
+@immutable
+class CloudNote {
+  final String documentId;
+  final String ownerUserId;
+  final String text;
+
+  const CloudNote({
+    required this.documentId,
+    required this.ownerUserId,
+    required this.text,
+  });
+
+  // give snapshot of cloudnote
+  CloudNote.fromSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
+      : documentId = snapshot.id,
+        ownerUserId = snapshot.data()[ownerUserIdFieldName],
+        text = snapshot.data()[textFieldName] as String;
+}
