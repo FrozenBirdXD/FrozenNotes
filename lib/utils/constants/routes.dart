2338fb45f3693f23c8f4460e88308e159eb8cdbc
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-04-27 21:12:00 +0200

    remove routes

--- lib/utils/constants/routes.dart
+++ lib/utils/constants/routes.dart
@@ -1,5 +1,1 @@
-const loginRoute = '/login/';
-const registerRoute = '/register/';
-const notesRoute = '/notes/home/';
-const verifyEmailRoute = '/verify-email/';
 const createOrUpdateNoteRoute = '/notes/update-create-note/';
