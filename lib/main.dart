9775200353f247bdf17cd8003da055264f9de8ea
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-04-28 16:33:53 +0200

    add FirebaseCrashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frozennotes/services/auth/bloc/auth_bloc.dart';
@@ -11,6 +13,16 @@ import 'package:frozennotes/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // catch errors thrown in flutter framework
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(
     MaterialApp(
       title: 'Frozennotes',
