8c63e63030ea5503a3af944fb1754058bef917d0
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-01 18:23:25 +0200

    fix test

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -113,7 +113,8 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'hello@world.com') throw UserNotFoundAuthException();
     if (password == 'hello') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: 'hello@world.com', id: 'my_id');
+    const user =
+        AuthUser(isEmailVerified: false, email: 'hello@world.com', id: 'my_id');
     _user = user;
     return Future.value(user);
   }
@@ -131,13 +132,20 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email : 'hello@world.com', id: 'my_id');
+    const newUser =
+        AuthUser(isEmailVerified: true, email: 'hello@world.com', id: 'my_id');
     _user = newUser;
   }
-  
+
   @override
   Future<void> sendPasswordReset({required String email}) async {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'Invalid@gmail') throw InvalidEmailAuthException();
+  }
+
+  @override
+  Future<void> changePassword({required String newPassword}) async {
+    if (!isInitialized) throw NotInitializedException();
+    if (newPassword == 'hi') throw WeakPasswordAuthException();
   }
 }
