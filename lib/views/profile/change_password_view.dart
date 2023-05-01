b380c0001da944a3f046b6c0c69d6dcd4ae4e321
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-01 19:13:07 +0200

    add change password view

---
+++ lib/views/profile/change_password_view.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frozennotes/services/auth/auth_exceptions.dart';
+import 'package:frozennotes/services/auth/bloc/auth_bloc.dart';
+import 'package:frozennotes/utils/dialogs/error_dialog.dart';
+
+class ChangePasswordView extends StatefulWidget {
+  const ChangePasswordView({super.key});
+
+  @override
+  State<ChangePasswordView> createState() => _ChangePasswordViewState();
+}
+
+class _ChangePasswordViewState extends State<ChangePasswordView> {
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _controller;
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) async {
+        if (state is AuthChangePasswordState) {
+          if (state.exception is WeakPasswordAuthException) {
+            await showErrorDialog(
+              context,
+              'Weak password - Please enter a stronger password.',
+            );
+          } else if (state.exception is RequiresRecentLogin) {
+            await showErrorDialog(
+              context,
+              'This operation requires you to have logged in recently. Please log in and try again.',
+            );
+          } else if (state.exception is GenericAuthException) {
+            await showErrorDialog(
+              context,
+              'Authentication error',
+            );
+          } else if (state.hasChangedPassword) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Password updated succesfully!'),
+                duration: Duration(seconds: 2),
+                behavior: SnackBarBehavior.floating,
+              ),
+            );
+          }
+        }
+      },
+      child: GestureDetector(
+        onTap: () {
+          // Hide the keyboard when the user taps outside of the TextFields
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Change Password'),
+            centerTitle: true,
+          ),
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 36.0,
+                  vertical: 48.0,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Enter a new password to update your current one.',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 32.0,
+                      ),
+                      TextFormField(
+                        controller: _controller,
+                        obscureText: !_passwordVisible,
+                        enableSuggestions: false,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[250],
+                          labelText: 'New Password',
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Theme.of(context).primaryColorDark,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                          ),
+                          labelStyle: const TextStyle(fontSize: 16.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey[300]!,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(
+                              color: Colors.blue[300]!,
+                            ),
+                          ),
+                        ),
+                        onFieldSubmitted: (value) {
+                          // hide keyboard when user taps register or presses enter
+                          FocusScope.of(context).unfocus();
+                          BlocProvider.of<AuthBloc>(context).add(
+                            AuthChangePasswordEvent(
+                                newPassword: _controller.text),
+                          );
+                        },
+                      ),
+                      const SizedBox(
+                        height: 32.0,
+                      ),
+                      // update password button
+                      SizedBox(
+                        width: double.infinity,
+                        height: 48.0,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // hide keyboard when user taps register or presses enter
+                            FocusScope.of(context).unfocus();
+                            BlocProvider.of<AuthBloc>(context).add(
+                              AuthChangePasswordEvent(
+                                  newPassword: _controller.text),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue[400],
+                            textStyle: const TextStyle(fontSize: 18.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                          child: const Text('Update Password'),
+                        ),
+                      ),
+                      // TODO: add back button
+                      // ElevatedButton(
+                      //   onPressed: () {
+                      //     BlocProvider.of<AuthBloc>(context).add(
+                      //       const AuthGoNotesEvent(),
+                      //     );
+                      //   },
+                      //   child: const Text('Go back'),
+                      // ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
