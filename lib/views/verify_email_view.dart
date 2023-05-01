bc6e9ec0f1df28962a2d53f96ff25b35aa7dbc76
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-01 10:55:56 +0200

    style verify email page

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frozennotes/services/auth/bloc/auth_bloc.dart';
@@ -14,35 +16,92 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Verify email'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Column(
-          children: [
-            const Text(
-              "We've sent you a verification email. Please open it to verify your account.",
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 36.0,
+              vertical: 48.0,
             ),
-            const Text(
-              "If you haven't received a verification yet, press the button below:",
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Verify your email address',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                const Text(
+                  "We've sent you a verification email. Please open it to verify your account.",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                const Text(
+                  "If you haven't received a verification yet, press the button below:",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(
+                  height: 32.0,
+                ),
+                // send verification email button
+                SizedBox(
+                  width: double.infinity,
+                  height: 48.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      BlocProvider.of<AuthBloc>(context)
+                          .add(const AuthSendVerificationEmailEvent());
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[400],
+                      textStyle: const TextStyle(fontSize: 18.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: const Text('Resend verification email'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                // go to login view button
+                SizedBox(
+                  width: double.infinity,
+                  height: 48.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      BlocProvider.of<AuthBloc>(context)
+                          .add(const AuthLogoutEvent());
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[200],
+                      textStyle: const TextStyle(fontSize: 18.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: const Text('Go back to login page'),
+                  ),
+                ),
+              ],
             ),
-            // send verification email button
-            ElevatedButton(
-              onPressed: () {
-                BlocProvider.of<AuthBloc>(context)
-                    .add(const AuthSendVerificationEmailEvent());
-              },
-              child: const Text('Resend verification email'),
-            ),
-            // go to login view button
-            ElevatedButton(
-              onPressed: () async {
-                BlocProvider.of<AuthBloc>(context).add(const AuthLogoutEvent());
-              },
-              child: const Text('Go to login page'),
-            ),
-          ],
+          ),
         ),
       ),
     );
