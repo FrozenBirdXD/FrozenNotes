44381e6ea03d8db12aa3864326c267461fa81642
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-05-01 10:55:35 +0200

    style forgot password page

--- lib/views/forgot_password_view.dart
+++ lib/views/forgot_password_view.dart
@@ -43,53 +43,114 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
         },
         child: Scaffold(
           resizeToAvoidBottomInset: false,
-          appBar: AppBar(title: const Text('Forgot Password')),
-          body: Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: Column(
-              children: [
-                const Text(
-                  "If you've lost your password or wish to reset it, simply enter your email address and follow the directions in the email.",
-                  style: TextStyle(fontSize: 20.0),
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 36.0,
+                  vertical: 48.0,
                 ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                // email text field
-                TextField(
-                  controller: _controller,
-                  autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                  enableSuggestions: false,
-                  decoration: const InputDecoration(
-                    hintText: 'Enter your email here',
-                  ),
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                // send email button
-                ElevatedButton(
-                  onPressed: () async {
-                    final email = _controller.text;
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Forgot Password? Don't Worry!",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    const Text(
+                      'Simply enter your email address and follow the directions in the email.',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32.0,
+                    ),
+                    // email text field
+                    TextFormField(
+                      controller: _controller,
+                      autocorrect: false,
+                      keyboardType: TextInputType.emailAddress,
+                      enableSuggestions: false,
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        filled: true,
+                        fillColor: Colors.grey[250],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide(
+                            color: Colors.grey[300]!,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide(
+                            color: Colors.blue[300]!,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32.0,
+                    ),
+                    // send email button
+                    SizedBox(
+                      width: double.infinity,
+                      height: 48.0,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          final email = _controller.text;
 
-                    FocusScope.of(context).unfocus(); // hide keyboard
-                    // login
-                    BlocProvider.of<AuthBloc>(context).add(
-                      AuthForgotPasswordEvent(email: email),
-                    );
-                  },
-                  child: const Text('Send password reset link'),
+                          FocusScope.of(context).unfocus(); // hide keyboard
+                          // send mail
+                          BlocProvider.of<AuthBloc>(context).add(
+                            AuthForgotPasswordEvent(email: email),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue[400],
+                          textStyle: const TextStyle(fontSize: 18.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        child: const Text('Send password reset link'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    // Go to login view button
+                    SizedBox(
+                      width: double.infinity,
+                      height: 48.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          BlocProvider.of<AuthBloc>(context)
+                              .add(const AuthLogoutEvent());
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue[200],
+                          textStyle: const TextStyle(fontSize: 18.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        child: const Text('Go back to login page'),
+                      ),
+                    ),
+                  ],
                 ),
-                // Go to register view button
-                ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<AuthBloc>(context)
-                        .add(const AuthLogoutEvent());
-                  },
-                  child: const Text('Go back to login page'),
-                ),
-              ],
+              ),
             ),
           ),
         ),
